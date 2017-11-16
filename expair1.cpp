fcbc828ea8c7ec29c5ecff51b033ade322b1279f
carlos-abraao <carlos.abraao.sa@gmail.com>
2017-11-16 16:03:52 -0300

    added pair example

--- expair1.cpp
+++ expair1.cpp
@@ -6,23 +6,42 @@ using namespace std;
  
 int main(){
 
+    cout << "\e[1;1H\e[2J";
+
     pair <string, int> g1;
     pair <string, int> g2("Teachers", 3);
     pair <string, int> g3(g2); 
     g1 = make_pair("Slaves", 40);
 
-    g2.first = "Tests";
+    g2.first  = "Tests";
     g2.second = 2;
  
     cout << "This is pair g1: " << g1.first << " " << g1.second <<  endl;
  
     cout << "This is pair g2: " << g2.first << " " << g2.second <<  endl;
 
-    cout << "This is pair g3: " << g3.first << " " << g3.second <<  endl;    
+    cout << "This is pair g3: " << g3.first << " " << g3.second <<  endl;
 
     swap(g1, g2);
 
-    cout << "After swapping, " << "g1 has " << g1.first << " and g2 has " << g2.first << endl;
+    cout << "Depois de swap:\n" << "g1 = " << g1.first << "  g2 = " << g2.first << endl;
+
+    //testes de comparação
+
+    pair<int, int>pair1 = make_pair(1, 12);
+    pair<int, int>pair2 = make_pair(9, 12);
+ 
+    //sempre definidos
+ 
+    cout << (pair1 == pair2) << endl;
+    cout << (pair1 != pair2) << endl;
+
+    //precisam de definiçõa pŕévia
+
+    cout << (pair1 >= pair2) << endl;
+    cout << (pair1 <= pair2) << endl;
+    cout << (pair1 > pair2)  << endl;
+    cout << (pair1 < pair2)  << endl;    
  
     return 0;
 }
